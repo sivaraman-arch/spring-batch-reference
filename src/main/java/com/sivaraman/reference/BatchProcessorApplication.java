c54f8f9fa7c66064cfd8f704c1a445210e489595
sivaraman <ssivaraman122@gmail.com>
2022-06-09 01:52:53 +0530

    created a simple 2 step ( tasklet ) based job

--- src/main/java/com/sivaraman/reference/BatchProcessorApplication.java
+++ src/main/java/com/sivaraman/reference/BatchProcessorApplication.java
@@ -1,9 +1,11 @@
 package com.sivaraman.reference;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchProcessorApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sivaraman/reference/config/SampleJob.java
@@ -0,0 +1,54 @@
+package com.sivaraman.reference.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleJob {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job firstJob(){
+        return jobBuilderFactory.get("First job").start(firstStep()).next(secondStep()).build();
+    }
+
+    @Bean
+    public Step firstStep(){
+        return stepBuilderFactory.get("First step").tasklet(firstTasklet()).build();
+    }
+
+    @Bean
+    public Tasklet firstTasklet(){
+
+        return (stepContribution, chunkContext) -> {
+           System.out.println("First tasklet");
+           return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Step secondStep(){
+        return stepBuilderFactory.get("Second step").tasklet(secondTasklet()).build();
+    }
+
+    public Tasklet secondTasklet(){
+        return (stepContribution, chunkContext) -> {
+            System.out.println("Second tasklet");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
