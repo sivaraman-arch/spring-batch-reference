f34392c8935571626da2aff4a58ad55a37b6f122
sivaraman <ssivaraman122@gmail.com>
2022-06-09 01:36:51 +0530

    feature(initial commit): Added required dependencies

---
+++ src/main/java/com/sivaraman/reference/BatchProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.sivaraman.reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivaraman/reference/BatchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivaraman.reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
